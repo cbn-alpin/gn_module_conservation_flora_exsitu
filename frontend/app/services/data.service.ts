0161609287444c3dcc1315dfc02299952d3d3388
BassirouD <moubass.diallo@gmail.com>
2025-03-27 09:56:24 +0100

    feat(data-service): add update and delete methods for harvest and materials

--- frontend/app/services/data.service.ts
+++ frontend/app/services/data.service.ts
@@ -19,16 +19,17 @@ export class DataService {
     return this.api.post<any>(`${this.moduleBaseUrl}/harvests`, data);
   }
 
-  // getAllHarvest() {
-  //   return this.api.get<any>(`${this.moduleBaseUrl}/harvests`);
-  // }
 
   getHarvestById(id_harvest) {
     return this.api.get<any>(`${this.moduleBaseUrl}/harvests/${id_harvest}`);
   }
 
-  addMaterial(data: any, id_harvest: number) {
-    return this.api.post<any>(`${this.moduleBaseUrl}/harvests/${id_harvest}/materials`, data);
+  deleteHarvest(id_harvest: number) {
+    return this.api.delete(`${this.moduleBaseUrl}/harvests/${id_harvest}`);
+  }
+
+  updateHarvest(id_harvest: number, data: any) {
+    return this.api.put(`${this.moduleBaseUrl}/harvests/${id_harvest}`, data);
   }
 
   deleteMaterial(id_material: number) {
@@ -42,6 +43,18 @@ export class DataService {
 
   getHarvestAll(params: HttpParams) {
     return this.api.get<any[]>(`${this.moduleBaseUrl}/harvests`, { params });
+  }
+
+  getHarvestGeometries(params: HttpParams){
+    return this.api.get<any>(`${this.moduleBaseUrl}/harvests/geometries`, {params});
+  }
+
+  addMaterial(data: any, id_harvest: number) {
+    return this.api.post<any>(`${this.moduleBaseUrl}/harvests/${id_harvest}/materials`, data);
+  }
+
+  updateMaterial(data: any, id_harvest: number, id_material: number) {
+    return this.api.put<any>(`${this.moduleBaseUrl}/harvests/${id_harvest}/materials/${id_material}`, data);
   }
 
   getFilteredCodes(query: string): Observable<string[]> {
@@ -62,8 +75,5 @@ export class DataService {
     return this.api.get<any>(`${this.moduleBaseUrl}/check-code-material?code_material=${codeMaterial}`);
   }
 
-  getHarvestGeometries(params: HttpParams){
-    return this.api.get<any>(`${this.moduleBaseUrl}/harvests/geometries`, {params});
-  }
 
 }
