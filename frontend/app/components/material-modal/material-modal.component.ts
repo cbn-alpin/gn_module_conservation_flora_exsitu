a9bf05dce0b6fe4f37b4bbee8083323e1a169b23
BassirouD <moubass.diallo@gmail.com>
2025-04-13 20:45:44 +0200

    refactor(front): rename id_harvest_material to id_material_type in material modal form

--- frontend/app/components/material-modal/material-modal.component.ts
+++ frontend/app/components/material-modal/material-modal.component.ts
@@ -36,7 +36,7 @@ export class MaterialModalComponent implements OnInit {
                 this.codeMaterialExists = false;
             }
         });
-        this.materialForm.controls['id_harvest_material'].valueChanges.subscribe(value => {   
+        this.materialForm.controls['id_material_type'].valueChanges.subscribe(value => {   
             if(value) {
               this.api.getCodesNomenclature(value).subscribe({
                 next: (code: string) => {
