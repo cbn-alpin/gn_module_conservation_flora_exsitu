3249fd249a2427acf25d0639e1cfacbb4fc38031
BassirouD <moubass.diallo@gmail.com>
2025-04-21 22:42:33 +0200

    feat(front): implement action list view for material

--- frontend/app/stock-management/actions-stock/actions-stock.component.ts
+++ frontend/app/stock-management/actions-stock/actions-stock.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { ActionModalComponent } from '../../components/action-modal/action-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ExsituFormService } from '../../form/shared/exsitu-form.service';
+import { DataService } from '../../services/data.service';
+import { HttpParams } from '@angular/common/http';
+
 
 @Component({
   selector: 'cfe-actions-stock',
@@ -9,13 +15,13 @@ import { MatPaginator } from '@angular/material/paginator';
 })
 export class ActionsStockComponent implements OnInit {
     @Input() title: string = '';
-    @Input() placeId!: number;
+    @Input() placeCode: string = '';
     dataSource = new MatTableDataSource<any>(); 
     @ViewChild(MatPaginator) paginator: MatPaginator;
     @ViewChild('dataTableContainer') dataTableContainer: ElementRef;
+    rowPerPage: number;
+    public totalActions: number;
     displayedColumns: string[] = [
-        'date_start', 
-        'actor', 
         'action_type',
         'quantity',
         'id_humidity_level',
@@ -24,14 +30,57 @@ export class ActionsStockComponent implements OnInit {
         'actions'
     ]; 
 
-    actions: any[] = [];
-
-    constructor() {}
+    constructor(
+      public dialog: MatDialog,
+      private exsituFormService: ExsituFormService,
+      private api: DataService
+    ) {}
 
     ngOnInit(): void {
-            // this.actionService.getActionsByPlace(this.placeId).subscribe((data) => {
-            //     this.actions = data;
-            // });
+      this.calculateNbRow()
+      this.loadActions();
     }
 
+    loadActions(){
+      const pageIndex = this.paginator ? this.paginator.pageIndex + 1 : 1;
+      const pageSize = this.paginator?.pageSize || this.rowPerPage || 10;
+      
+      let params = new HttpParams()
+                  .set('page', pageIndex)
+                  .set('limit', pageSize)
+                  .set('placeCode', this.placeCode);   
+      this.api.getActions(this.exsituFormService.idMaterial, params).subscribe({
+        next: (data) => {     
+          console.log(data);
+                         
+          this.dataSource.data = data['items'];
+          this.totalActions = data['total'];            
+        },
+        error: (err) => {
+          console.error('Erreur lors de la récupération des actions', err);
+        }
+      })
+    }
+
+    openActionModal(): void {
+      const data = {id_material: this.exsituFormService.idMaterial, placeCode: this.placeCode}      
+      const dialogRef = this.dialog.open(ActionModalComponent, {
+        width: '70%',
+        height: '80%',
+        data: { data: data }
+      });
+      dialogRef.afterClosed().subscribe(() => {
+        this.loadActions();
+      });
+    }
+
+    calculateNbRow() {
+      let wH = window.innerHeight;
+      let listHeight = wH - 400;
+      this.rowPerPage = Math.round(listHeight / 170);
+    }
+
+    onPaginateChange(){
+      this.loadActions();
+    }
 }
