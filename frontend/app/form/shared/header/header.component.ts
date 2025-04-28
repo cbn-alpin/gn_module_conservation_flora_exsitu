98ea85d7a89ef487a629004994eb176b646b8c9b
BassirouD <moubass.diallo@gmail.com>
2025-04-28 11:00:14 +0200

    feat(front): add code material in header

--- frontend/app/form/shared/header/header.component.ts
+++ frontend/app/form/shared/header/header.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ExsituFormService } from '../exsitu-form.service';
 import { DataService } from '../../../services/data.service';
 
@@ -9,8 +9,10 @@ import { DataService } from '../../../services/data.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
     harvest: any;
+    codeMaterial: string;
+    private subscriptions = [];
 
     constructor(
         public exsituFormService: ExsituFormService,
@@ -20,11 +22,40 @@ export class HeaderComponent implements OnInit {
     }
     ngOnInit(): void {
       this.loadHarvest(this.exsituFormService.idHarvest)
+      if (this.exsituFormService.idMaterial) {
+        this.loadCodeMaterial();
+      }
+  
+      const sub = this.exsituFormService.idMaterialChange.subscribe(idMaterial => {
+        if (idMaterial) {
+          this.loadCodeMaterial();
+        } else {
+          this.codeMaterial = null;
+        }
+      });
+      this.subscriptions.push(sub);
     }
 
     loadHarvest(id_harvest){
       this.api.getHarvestInfos(id_harvest).subscribe((harvest) => {
         this.harvest = harvest
       });
+    }
+
+    loadCodeMaterial(){
+      if (this.exsituFormService.idMaterial) {
+        this.api.getMaterialInfos(this.exsituFormService.idMaterial).subscribe({
+          next: (material) => {
+            this.codeMaterial = material.code_material;            
+          },
+          error: (err) => {
+            console.error('Erreur lors de la récupération du matériel:', err);
+          }
+        });
+      }
+    }
+
+    ngOnDestroy() {
+      this.subscriptions.forEach(sub => sub.unsubscribe());
     }
 }
