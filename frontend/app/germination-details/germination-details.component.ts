c134c4f7d8b7097e5b7dca5ce452fe843c653571
Aïcha EL JILI <a.eljili@cbn-alpin.fr>
2025-07-29 23:39:10 +0200

    refactor(ui): update germination test detail view

--- frontend/app/germination-details/germination-details.component.ts
+++ frontend/app/germination-details/germination-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
-import { Router } from '@angular/router';
-import { MatDialogRef } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../services/data.service';
+import { ExsituFormService } from '../form/shared/exsitu-form.service';
 
 interface Germination {
   numSemis: string;
@@ -12,13 +13,19 @@ interface Germination {
   replicate: number;
   levage: number;
 }
+
 @Component({
   selector: 'app-germination-details',
   templateUrl: './germination-details.component.html',
   styleUrls: ['./germination-details.component.scss']
 })
 export class GerminationDetailsComponent implements OnInit {
+  idMaterial!: number;
+  idStorage:any;
+  idTest!: number;
   selectedAction: any = null;
+  labels: any = {};
+
   germinationForm: FormGroup;
   dataSource = new MatTableDataSource<Germination>([]);
   displayedColumns: string[] = [
@@ -30,107 +37,119 @@ export class GerminationDetailsComponent implements OnInit {
     'levage'
   ];
 
-  constructor(private fb: FormBuilder,public router: Router ) {
+  constructor(
+    private fb: FormBuilder,
+    public router: Router,
+    private route: ActivatedRoute,
+    private api: DataService,
+    private exsituFormService: ExsituFormService
+  ) {
     this.germinationForm = this.fb.group({
-      reference: ['', Validators.required],
-      provenance: ['', Validators.required],
-      numeroSemis: ['', Validators.required],
-      numeroSemence: ['', Validators.required],
-      preparation: [''],
-      contenant: [''],
-      substrat: [''],
-      arrosage: [''],
-      modeSemis: [''],
-      profondeur: [''],
-      dateDebut: ['', Validators.required],
-      dateFin: [''],
-      traitement: [''],
-      remarques: [''],
-      replicats: this.fb.array([this.createReplicat()])
-    });
-  }
-  get replicats(): FormArray {
-    return this.germinationForm.get('replicats') as FormArray;
-  }
-  addNewGermination(){
-  }
-
-
-  get replicatsControls() {
-    return (this.germinationForm.get('replicats') as FormArray).controls;
-  }
-
-  createReplicat(): FormGroup {
-    return this.fb.group({
-      date: [''],
-      plantulesLevees: [''],
-      plantulesMortes: [''],
-      plantulesRepiques: [''],
-      grainesSemees: ['']
+      code: [''],
+      id_test_parent: [''],
+      id_material: [''],
+      id_actor: [''],
+      id_storage: [''],
+      id_test_type: [''],
+      seed_initial_count: [''],
+      replicate_count: [''],
+      id_support: [''],
+      id_substrate: [''],
+      remarks: [''],
+      additional_data: this.fb.group({
+        program: ['']
+      })
     });
   }
 
-  addReplicat(): void {
-    (this.germinationForm.get('replicats') as FormArray).push(this.createReplicat());
-  }
-  onBack(){
-    window.history.back();
-
-  }
-
-  onActionSelected(action: any) {
-    this.selectedAction = action;
-  }
   ngOnInit(): void {
-    this.germinationForm.patchValue({
-      reference: 'Contrat ABC',
-      provenance: 'Test 123',
-      numeroSemis: 'SEM123',
-      numeroSemence: 'SEED456',
-      preparation: 'Some remarks',
-      contenant: 'Scarification process',
-      substrat: 'Peat moss'
+    this.idTest = +(this.route.snapshot.paramMap.get('id_test') || 0);
+    this.idMaterial = +(this.route.snapshot.paramMap.get('idMaterial') || 0);
+
+    if (!this.idTest && this.exsituFormService.idTest) {
+      this.idTest = this.exsituFormService.idTest;
+    }
+    if (!this.idMaterial && this.exsituFormService.idMaterial) {
+      this.idMaterial = this.exsituFormService.idMaterial;
+    }
+    if (!this.idStorage && this.exsituFormService.idStorage) {
+      this.exsituFormService.id_storage.subscribe((id) => {
+        this.idStorage = id;
+      });
+    }
+
+    console.log("🆔 id_test:", this.idTest);
+    console.log("🧪 id_material:", this.idMaterial);
+
+    if (!this.idTest || !this.idMaterial) {
+      console.error("❌ idTest ou idMaterial manquant !");
+      return;
+    }
+
+    this.loadTestDetails();
+
+  }
+ 
+  loadTestDetails(): void {
+    this.api.getTestWithLabels(this.idTest).subscribe({
+      next: (test) => {
+        console.log(" Test chargé :", test);
+        this.patchFormFromTest(test);
+        this.dataSource.data = test.actions || [];
+      },
+      error: (err) => {
+        console.error(" Erreur lors du chargement du test :", err);
+      }
     });
   }
 
-  onDelete(){
+  patchFormFromTest(test: any): void {
+    this.germinationForm.patchValue({
+      code: test.code,
+      id_test_parent: test.id_test_parent || '',
+      id_material: test.material_label,
+      id_actor: test.actor_label,
+      id_storage: test.storage_label || '',
+      id_test_type: test.test_type_label,
+      seed_initial_count: test.seed_initial_count,
+      replicate_count: test.replicate_count,
+      id_support: test.support_label,
+      id_substrate: test.substrate_label,
+      remarks: test.remarks,
+      additional_data: {
+        program: test.additional_data?.program || ''
+      }
+    });
+  }
 
-  }
-  onView(){
-    
-  }
-  onEdit(){
-    
+  onBack(): void {
+    window.history.back();
   }
 
-  onCancel(){
-    
+  onActionSelected(action: any): void {
+    const idAction = action.id_action;
+  
+    // Recharge les données complètes de l'action depuis l'API
+    this.api.getActionWithLabels(idAction).subscribe({
+      next: (fullAction) => {
+        this.selectedAction = fullAction;  // contient tous les champs + labels
+      },
+      error: (err) => {
+        console.error("Erreur chargement de l'action :", err);
+      }
+    });
   }
-  onSubmit() {
+  
+
+  onDelete(): void {}
+  onView(): void {}
+  onEdit(): void {}
+  onCancel(): void {}
+
+  onSubmit(): void {
     if (this.germinationForm.valid) {
       const formData = this.germinationForm.value;
       console.log('Formulaire soumis :', formData);
-     // this.dialogRef.close(formData); // ferme le modal et renvoie les données
     }
   }
-  // onSubmit() {
-  //   // if (this.germinationForm.valid) {
-  //   //   // Create new Semis entry from form values
-  //   //   const newEntry: Germination = {
-  //   //     numSemis: this.germinationForm.value.numeroSemis,
-  //   //     numSemence: this.germinationForm.value.numeroSemence,
-  //   //     dateDebut: this.germinationForm.value.dateDebut,
-  //   //     dateFin: this.germinationForm.value.dateFin,
-  //   //     replicate: 0, // Add actual value mapping
-  //   //     levage: 0    // Add actual value mapping
-  //   //   };
-
-  //     // // Add to table
-  //     // this.dataSource.data = [...this.dataSource.data, newEntry];
-      
-  //     // // Reset form
-  //     // this.germinationForm.reset();
-  //     this.router.navigate([`conservation_flora_exsitu/germination-table`]);
-
-  //   }
-  } 
+}
