bcad9b66fe451be33a58158fb184dd02c238fa61
Aïcha EL JILI <a.eljili@cbn-alpin.fr>
2025-07-30 21:37:31 +0200

    feat(ui): add viability test modal component

--- frontend/app/viability/viability.component.ts
+++ frontend/app/viability/viability.component.ts
@@ -1,7 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ConfigService } from '../services/config.service';
+import { ActivatedRoute } from '@angular/router';
+import { ExsituFormService } from '../form/shared/exsitu-form.service';
+import { FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { map, startWith, switchMap, debounceTime } from 'rxjs/operators';
+import { DataService } from '../services/data.service';
+import { CommonService } from '@geonature_common/service/common.service';  
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ViabilityFormService } from './viability-form.service';
+
 
 interface Viability {
   numSemis: string;
@@ -18,6 +30,7 @@ interface Viability {
 })
 export class ViabilityComponent implements OnInit {
 
+ 
   germinationForm: FormGroup;
   dataSource = new MatTableDataSource<Viability>([]);
   displayedColumns: string[] = [
@@ -28,74 +41,200 @@ export class ViabilityComponent implements OnInit {
     'replicate',
     'levage'
   ];
+  codeT: any= 'via'
+  idGermination:any;
+  idStorage:any;
+  public observers_list_code;
+  additionalDataForm: FormGroup;
+  formsDefinition;
 
-  constructor(private fb: FormBuilder,public dialogRef: MatDialogRef<ViabilityComponent> ) {
+  codeTest = new FormControl();
+  filteredTest$: Observable<string[]>;
+
+  constructor(
+    public router: Router,
+    public dialogRef: MatDialogRef<ViabilityComponent>,
+    public germinationFormService: ViabilityFormService,
+    public cfg: ConfigService,
+    private route: ActivatedRoute,
+    private exsituFormService: ExsituFormService,
+    private fb: FormBuilder,
+    public api: DataService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+
+    private _commonService: CommonService,
+
+
+   ) {
     this.germinationForm = this.fb.group({
       code: ['', Validators.required],
-      idTestParent: [null, Validators.required], 
-      idMaterial: [null, Validators.required],
-      idActor: [null, Validators.required],
-      idTestType: [null, Validators.required], 
-      idStorage: [null], 
-      seedInitialCount: [null],
-      replicateCount: [1],
-      idSubstrate: [null],
-      idSupport: [null],
-      program: [''],
-      remarks: ['']
+      // id_test_parent: ['', Validators.required],  
+      // id_material: ['', Validators.required],    
+      id_actor: [''],
+      // id_test_type: [''],
+      seed_initial_count: [null, Validators.required],                         
+      replicate_count: [1, Validators.required],                       
+      id_substrate: [null, Validators.required],
+      id_support: [null, Validators.required],
+      remarks: [''],
+      additional_data: this.fb.group({}),
+      
     });
   }
-  get replicats(): FormArray {
-    return this.germinationForm.get('replicats') as FormArray;
-  }
-  addNewGermination(){
-  }
-
-
-  get replicatsControls() {
-    return (this.germinationForm.get('replicats') as FormArray).controls;
-  }
-
-  createReplicat(): FormGroup {
-    return this.fb.group({
-      date: [''],
-      plantulesLevees: [''],
-      plantulesMortes: [''],
-      plantulesRepiques: [''],
-      grainesSemees: ['']
+  patchForm(test: any): void {
+    this.germinationForm.patchValue({
+      code: test.code || '',
+      id_storage: this.idStorage || '',                             
+      id_material: test.id_material || '',
+      id_actor: test.id_actor ? [ { id_role: test.id_actor } ] : [],
+      seed_initial_count: test.seed_initial_count ?? null,
+      replicate_count: test.replicate_count ?? null,
+      id_substrate: test.id_substrate ?? null,
+      id_support: test.id_support ?? null,
+      remarks: test.remarks || ''
     });
-  }
-
-  addReplicat(): void {
-    (this.germinationForm.get('replicats') as FormArray).push(this.createReplicat());
+  
+    if (test.additional_data && this.additionalDataForm) {
+      Object.keys(test.additional_data).forEach(key => {
+        if (this.additionalDataForm.contains(key)) {
+          this.additionalDataForm.get(key)?.patchValue(test.additional_data[key]);
+        }
+      });
+    }
   }
   
 
+  idMaterial: number;
+  idSeed: number;
   ngOnInit(): void {
-    // Initialize with sample data if needed
+    this.filteredTest$ = this.codeTest.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+      switchMap(value => this.api.getMaterialsCodeParent(this.exsituFormService.idHarvest).pipe(
+        map(materials => materials.filter(material =>
+          material.code_material.toLowerCase().includes(value.toLowerCase())
+        ))
+      ))
+    );
+  
+    this.idMaterial = this.exsituFormService.idMaterial;
+    this.idSeed = this.exsituFormService.idSeed;
+
     this.dataSource.data = [];
+  
+    this.observers_list_code = this.cfg.getObsCode();
+  
+    this.additionalDataForm = this.germinationForm.get('additional_data') as FormGroup;
+    this.formsDefinition = this.cfg.getModuleConfigExsitu()['harvest_form']['additional_data'];
+  
+    if (this.formsDefinition && this.additionalDataForm) {
+      this.formsDefinition.forEach(field => {
+        const fieldName = field.attribut_name;
+        this.additionalDataForm.addControl(fieldName, this.fb.control(''));
+      });
+    }
+  
+    this.getTestByCode(this.codeT);
+  
+    if (this.data?.edit && this.data?.test) {
+      console.log("📦 Test reçu pour édition :", this.data.test);
+      this.patchForm(this.data.test);
+    }
+
+    this.exsituFormService.id_storage.subscribe((id) => {
+      this.idStorage = id;
+    });
+    console.log(this.idStorage)
+  }
+  
+  getTestByCode(code :any): void {
+    this.api.getActionByCode(code).subscribe({
+      next: (test) => {
+        this.idGermination=test.id_nomenclature
+        console.log(this.idGermination)
+        console.log(test)
+        console.log(test.id_nomenclature)
+      },
+      error: (err) => {
+        console.error("Erreur lors du chargement du code :", err);
+      }
+    });
+  }
+  
+  private formatDataFormHarvest() {
+    const finalForm = JSON.parse(JSON.stringify(this.germinationForm.value));
+
+    const additionalFields = this.formsDefinition || [];
+
+    if (finalForm.additional_data) {
+      const cleanedAdditionalData = {};
+    
+      additionalFields.forEach(field => {
+        const key = field.attribut_name;
+        const value = finalForm.additional_data[key];
+        if (value !== null && value !== undefined && value !== '') {
+          cleanedAdditionalData[key] = value;
+        }
+      });
+    
+      if (Object.keys(cleanedAdditionalData).length > 0) {
+        finalForm.additional_data = cleanedAdditionalData;
+      } else {
+        delete finalForm.additional_data;
+      }
+    }
+    if(this.codeTest){
+      finalForm.code_parent = this.codeTest.value;
+    }
+    finalForm['id_actor'] = finalForm['id_actor'][0].id_role;
+    finalForm['id_material'] = this.idMaterial;
+    finalForm['id_test_type'] = this.idGermination;
+    finalForm['id_storage'] = this.idStorage;
+    
+    return finalForm;
+
+  }
+  onDelete(): void {
   }
 
-  onDelete(){
+  onCancel(): void {
+    this.dialogRef.close();
+  }
 
+  onView(): void {
   }
-  onView(){
-    
+
+  onEdit(): void {
   }
-  onEdit(){
-    
-  }
-  onSubmit() {
-    if (this.germinationForm.valid) {
-      const formData = this.germinationForm.value;
-      console.log('Formulaire soumis :', formData);
-      this.dialogRef.close(formData); // ferme le modal et renvoie les données
+  onSubmit(): void {
+    const finalForm = this.formatDataFormHarvest();
+  
+    if (!this.idMaterial) {
+      console.error("idMaterial est manquant !");
+      return;
+    }
+  
+    if (this.data?.edit && this.data?.test?.id_test) {
+      this.api.updateTest(this.idMaterial, this.data.test.id_test, finalForm).subscribe({
+        next: (res) => {
+          this._commonService.translateToaster('success', 'Test mis à jour avec succès');
+          this.dialogRef.close(res);
+        },
+        error: (err) => {
+          console.error("Erreur lors de la mise à jour du test :", err);
+        }
+      });
+    } else {
+      this.api.createTest(finalForm, this.idMaterial).subscribe({
+        next: (res) => {
+          this._commonService.translateToaster('info', 'Test créé avec succès');
+          this.dialogRef.close(res);
+        },
+        error: (err) => {
+          console.error("Erreur lors de la création du test :", err);
+        }
+      });
     }
   }
-
-
-  onCancel(){
-    this.dialogRef.close();
-
-  }
+  
 }
