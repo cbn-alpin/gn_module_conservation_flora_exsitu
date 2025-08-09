c26e86a32af1a91d2a6b695da552e8bb92fcd90a
Aïcha EL JILI <a.eljili@cbn-alpin.fr>
2025-08-09 22:01:14 +0200

    feat(ui): display viability test details

--- frontend/app/viability-details/viability-details.component.ts
+++ frontend/app/viability-details/viability-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
-import { Router } from '@angular/router';
-import { MatDialogRef } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../services/data.service';
+import { ExsituFormService } from '../form/shared/exsitu-form.service';
 
 interface Viability {
   numSemis: string;
@@ -18,8 +19,11 @@ interface Viability {
   styleUrls: ['./viability-details.component.scss']
 })
 export class ViabilityDetailsComponent implements OnInit {
-
+idMaterial!: number;
+  idStorage:any;
+  idTest!: number;
   selectedAction: any = null;
+  labels: any = {};
 
   germinationForm: FormGroup;
   dataSource = new MatTableDataSource<Viability>([]);
@@ -32,80 +36,119 @@ export class ViabilityDetailsComponent implements OnInit {
     'levage'
   ];
 
-  constructor(private fb: FormBuilder,public router: Router ) {
+  constructor(
+    private fb: FormBuilder,
+    public router: Router,
+    private route: ActivatedRoute,
+    private api: DataService,
+    private exsituFormService: ExsituFormService
+  ) {
     this.germinationForm = this.fb.group({
       code: [''],
-      idTestParent: [null],
-      idMaterial: [null],
-      idActor: [null],
-      idTestType: [null],
-      idStorage: [null],
-      seedInitialCount: [null],
-      replicateCount: [1],
-      idSubstrate: [null],
-      idSupport: [null],
-      program: [''],
+      id_test_parent: [''],
+      id_material: [''],
+      id_actor: [''],
+      id_storage: [''],
+      id_test_type: [''],
+      seed_initial_count: [''],
+      replicate_count: [''],
+      id_support: [''],
+      id_substrate: [''],
       remarks: [''],
-    });
-  }
-  get replicats(): FormArray {
-    return this.germinationForm.get('replicats') as FormArray;
-  }
-  addNewGermination(){
-  }
-
-  onActionSelected(action: any) {
-    this.selectedAction = action;
-  }
-
-  get replicatsControls() {
-    return (this.germinationForm.get('replicats') as FormArray).controls;
-  }
-
-  createReplicat(): FormGroup {
-    return this.fb.group({
-      date: [''],
-      plantulesLevees: [''],
-      plantulesMortes: [''],
-      plantulesRepiques: [''],
-      grainesSemees: ['']
+      additional_data: this.fb.group({
+        program: ['']
+      })
     });
   }
 
-  addReplicat(): void {
-    (this.germinationForm.get('replicats') as FormArray).push(this.createReplicat());
+  ngOnInit(): void {
+    this.idTest = +(this.route.snapshot.paramMap.get('id_test') || 0);
+    this.idMaterial = +(this.route.snapshot.paramMap.get('idMaterial') || 0);
+
+    if (!this.idTest && this.exsituFormService.idTest) {
+      this.idTest = this.exsituFormService.idTest;
+    }
+    if (!this.idMaterial && this.exsituFormService.idMaterial) {
+      this.idMaterial = this.exsituFormService.idMaterial;
+    }
+    if (!this.idStorage && this.exsituFormService.idStorage) {
+      this.exsituFormService.id_storage.subscribe((id) => {
+        this.idStorage = id;
+      });
+    }
+
+    console.log("🆔 id_test:", this.idTest);
+    console.log("🧪 id_material:", this.idMaterial);
+
+    if (!this.idTest || !this.idMaterial) {
+      console.error("❌ idTest ou idMaterial manquant !");
+      return;
+    }
+
+    this.loadTestDetails();
+
+  }
+ 
+  loadTestDetails(): void {
+    this.api.getTestWithLabels(this.idTest).subscribe({
+      next: (test) => {
+        console.log(" Test chargé :", test);
+        this.patchFormFromTest(test);
+        this.dataSource.data = test.actions || [];
+      },
+      error: (err) => {
+        console.error(" Erreur lors du chargement du test :", err);
+      }
+    });
+  }
+
+  patchFormFromTest(test: any): void {
+    this.germinationForm.patchValue({
+      code: test.code,
+      id_test_parent: test.id_test_parent || '',
+      id_material: test.material_label,
+      id_actor: test.actor_label,
+      id_storage: test.storage_label || '',
+      id_test_type: test.test_type_label,
+      seed_initial_count: test.seed_initial_count,
+      replicate_count: test.replicate_count,
+      id_support: test.support_label,
+      id_substrate: test.substrate_label,
+      remarks: test.remarks,
+      additional_data: {
+        program: test.additional_data?.program || ''
+      }
+    });
+  }
+
+  onBack(): void {
+    window.history.back();
+  }
+
+  onActionSelected(action: any): void {
+    const idAction = action.id_action;
+  
+    // Recharge les données complètes de l'action depuis l'API
+    this.api.getActionWithLabels(idAction).subscribe({
+      next: (fullAction) => {
+        this.selectedAction = fullAction;  // contient tous les champs + labels
+      },
+      error: (err) => {
+        console.error("Erreur chargement de l'action :", err);
+      }
+    });
   }
   
 
-  ngOnInit(): void {
+  onDelete(): void {}
+  onView(): void {}
+  onEdit(): void {}
+  onCancel(): void {}
 
-    this.dataSource.data = [];
-  }
-
-  onDelete(){
-
-  }
-  onView(){
-    
-  }
-  onEdit(){
-    
-  }
-  onBack(){
-    window.history.back();
-
-  }
-
-  onCancel(){
-    
-  }
-  onSubmit() {
+  onSubmit(): void {
     if (this.germinationForm.valid) {
       const formData = this.germinationForm.value;
       console.log('Formulaire soumis :', formData);
-     // this.dialogRef.close(formData); // ferme le modal et renvoie les données
     }
   }
-  
-
-  } 
+}
